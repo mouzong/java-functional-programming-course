e6b687011a8dbe48c9aef30bc6368422aa924e3f
mouzong <andreasmouzong@gmail.com>
2022-03-08 05:04:06 +0100

    understanding chaining function

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -4,15 +4,26 @@ import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
-    int increment = increment(1);
+    int increment = incrementByOne(1);
         System.out.println(increment);
+
         int increment2 = incrementByOneFunction.apply(1);
         System.out.println(increment2);
+
+        int multiply = multiplyBy10Function.apply(increment2);
+        System.out.println(multiply);
+
+        int chained = addBy1ThenMultiplyBy10.apply(increment);
+        System.out.println(chained);
     }
 
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
 
-    static int increment(int number){
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
+    static Function<Integer, Integer> addBy1ThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
+
+    static int incrementByOne(int number){
         return  number + 1;
     }
 }
