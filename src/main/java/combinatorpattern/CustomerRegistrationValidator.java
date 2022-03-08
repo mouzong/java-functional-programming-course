ccb1cd79ae56985566339e3295f9370ec602e81d
mouzong <andreasmouzong@gmail.com>
2022-03-08 15:00:45 +0100

    Combinatory pattern in action

---
+++ src/main/java/combinatorpattern/CustomerRegistrationValidator.java
@@ -0,0 +1,42 @@
+package combinatorpattern;
+
+import com.sun.net.httpserver.Authenticator;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.function.Function;
+
+import static combinatorpattern.CustomerRegistrationValidator.*;
+import static combinatorpattern.CustomerRegistrationValidator.ValidationResult.*;
+
+public interface CustomerRegistrationValidator
+        extends Function<Customer, ValidationResult> {
+    static CustomerRegistrationValidator isEmailValid () {
+        return customer -> customer.getEmail().contains("@") ?
+                SUCCESS : EMAIL_NOT_VALID;
+    }
+
+    static CustomerRegistrationValidator isPhoneNumberValid () {
+        return customer -> customer.getPhoneNumber().startsWith("+0") ?
+                SUCCESS : PHONE_NUMBER_NOTVALID;
+    }
+
+    static CustomerRegistrationValidator isAdult () {
+        return customer ->
+                Period.between(customer.getDateOfBirth(), LocalDate.now()).getYears() > 16 ?
+                SUCCESS : IS_NOT_ADULT;
+    }
+
+    default CustomerRegistrationValidator and (CustomerRegistrationValidator other){
+        return customer -> {
+            ValidationResult result = this.apply(customer);
+            return result.equals(SUCCESS) ? other.apply(customer) : result;
+        };
+    }
+    enum ValidationResult{
+        SUCCESS,
+        PHONE_NUMBER_NOTVALID,
+        EMAIL_NOT_VALID,
+        IS_NOT_ADULT
+    }
+}
--- src/main/java/combinatorpattern/Main.java
+++ src/main/java/combinatorpattern/Main.java
@@ -7,12 +7,25 @@ public class Main {
         Customer customer = new Customer(
                 "Alice",
                 "alice@gmail.com",
-                "0697332174",
+                "+0697332174",
                 LocalDate.of(2000, 1,1)
         );
 
-        CustomerValidatorService validatorService = new CustomerValidatorService();
+        // CustomerValidatorService validatorService = new CustomerValidatorService();
 
-        System.out.println(validatorService.isValidCustomer(customer));
+        // System.out.println(validatorService.isValidCustomer(customer));
+
+        // Using combinatory design pattern
+        CustomerRegistrationValidator.ValidationResult result = CustomerRegistrationValidator
+                .isEmailValid()
+                .and(CustomerRegistrationValidator.isPhoneNumberValid())
+                .and(CustomerRegistrationValidator.isAdult())
+                .apply(customer);
+
+        System.out.println(result);
+
+        if(result != CustomerRegistrationValidator.ValidationResult.SUCCESS){
+            throw new IllegalStateException(result.name());
+        }
     }
 }
