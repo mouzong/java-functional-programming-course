bc2f47c88623eec2b6b52e060594ee9d4779633e
mouzong <andreasmouzong@gmail.com>
2022-03-08 07:38:58 +0100

    undeerstanding Supplier<T>

--- src/main/java/functionalinterface/_Predicates.java
+++ src/main/java/functionalinterface/_Predicates.java
@@ -6,9 +6,15 @@ public class _Predicates {
     public static void main(String[] args) {
         System.out.println(isPhoneNumberValid("07000000000"));
         System.out.println(isPhoneNumberValidPredicate.test("09000000000"));
+
+        // chaining predicates
+        System.out.println(isPhoneNumberValidPredicate.and(contains3).test("09000000003"));
+
+        System.out.println(isPhoneNumberValidPredicate.and(contains3).test("07000000030"));
     }
 
 
+    static Predicate<String> contains3 = phoneNumber -> phoneNumber.contains("3");
     static Predicate<String> isPhoneNumberValidPredicate = phoneNumber -> phoneNumber.startsWith("07") && phoneNumber.length() == 11;
 
     static boolean isPhoneNumberValid(String phoneNumber){
